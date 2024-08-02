5ca86797b12e3ab57bb99522f06be1204fe16bb0
andrelbastida <andrelbastida@yahoo.com.br>
2024-08-01 22:28:59 -0300

    feat: add maintenance page with integrated index view
    - Created a maintenance page to handle equipment details and image uploads.
    - Integrated maintenance page functionality into the existing index page for visualization.
    - Updated form fields and styling for improved user experience.

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,69 +1,64 @@
-import { Footer } from "../app/components/footer";
-import { Aside } from "../app/components/aside";
-import { Card } from "../app/components/card";
-import { FaBuilding } from "react-icons/fa";
-import { Tabela } from "../app/components/table";
-import { Grafico } from "../app/components/graphics";
-import Layout from '../app/layout';
+import { Grafico } from '@/app/components/graphics';
 import '../app/globals.css';
+import { Tabela } from '@/app/components/table';
+import { Footer } from '@/app/components/footer';
+import { Card } from '@/app/components/card';
+import { Aside } from '@/app/components/aside';
+import { FaBuilding } from 'react-icons/fa';
+// Pagina de Maintenance
 
 
-export default function Index() {
-  const cards = [
-    {
-      color: "bg-orange-200",
-      qty: "100",
-      text: "Ambientes",
-      icon: <FaBuilding size={48} />,
-    },
-    {
-      color: "bg-blue-200",
-      qty: "100",
-      text: "Ambientes",
-      icon: <FaBuilding size={48} />,
-    },
-    {
-      color: "bg-red-200",
-      qty: "100",
-      text: "Ambientes",
-      icon: <FaBuilding size={48} />,
-    },
-    {
-      color: "bg-yellow-200",
-      qty: "100",
-      text: "Ambientes",
-      icon: <FaBuilding size={48} />,
-    },
-  ];
+export default function Index(){
   return (
     <div className="h-screen flex flex-col">
       <div className="flex-1 flex">
-        <Aside />
+        <Aside/>
 
         <main className="flex-1 flex-col flex text-black max-h-svh">
           <h1 className="text-2xl font-bold uppercase w-full bg-white/80 p-3 text-center">
-            Sistema de Gestão de Manutenção
+            Cadastro de Maquinário
           </h1>
-
           <div>
-            <div className="grid grid-cols-4 gap-4 p-6">
-              {cards.map((props) => (
-                <Card
-                  color={props.color}
-                  qty={props.qty}
-                  text={props.text}
-                  icon={props.icon}
-                />
-              ))}
+          <form  className="space-y-4 form-data">
+            <div>
+                <label >Nome da Máquina</label>
+                <input />
             </div>
+
+            <div>
+                <label >Tipo</label>
+                <input />
+            </div>
+
+            <div>
+                <label >Modelo</label>
+                <input />
+            </div>
+
+            <div>
+                <label >Data de Fabricação</label>
+                <input />
+            </div>
+
+            <div>
+                <label >Número de série</label>
+                <input />
+            </div>
+
+            <div>
+                <label >Localização</label>
+                <input />
+            </div>
+
+            <div>
+                <button className="w-full py-2 px-4 bg-indigo-600 text-white font-semibold rounded-md shadow-md hover:bg-indigo-700 focus:ring focus:ring-opacity-50 focus:ring-indigo-300">Submit</button>
+            </div>
+        </form>
           </div>
-          <div className="flex flex-1">
-            <Grafico />
-            <Tabela />
-          </div>
+          
         </main>
       </div>
-      <Footer />
+      
     </div>
   );
 }
--- frontend/src/pages/maintenance.tsx
+++ frontend/src/pages/maintenance.tsx
@@ -1,9 +1,72 @@
-export default function PageMaintenance(){
-    return(
-        <div>
-            <h1>Testando mod</h1>
-        </div>
+import { Footer } from "../app/components/footer";
+import { Aside } from "../app/components/aside";
+import { Card } from "../app/components/card";
+import { FaBuilding } from "react-icons/fa";
+import { Tabela } from "../app/components/table";
+import { Grafico } from "../app/components/graphics";
+import Layout from '../app/layout';
+import '../app/globals.css';
 
-    )
+//   PÁGINA Original !!!!!!!!!
 
+export default function Index() {
+  const cards = [
+    {
+      color: "bg-orange-200",
+      qty: "100",
+      text: "Ambientes",
+      icon: <FaBuilding size={48} />,
+    },
+    {
+      color: "bg-blue-200",
+      qty: "100",
+      text: "Ambientes",
+      icon: <FaBuilding size={48} />,
+    },
+    {
+      color: "bg-red-200",
+      qty: "100",
+      text: "Ambientes",
+      icon: <FaBuilding size={48} />,
+    },
+    {
+      color: "bg-yellow-200",
+      qty: "100",
+      text: "Ambientes",
+      icon: <FaBuilding size={48} />,
+    },
+  ];
+  //   PÁGINA Original !!!!!!!!!
+  return (
+    <div className="h-screen flex flex-col">
+      <div className="flex-1 flex">
+        <Aside />
+
+        <main className="flex-1 flex-col flex text-black max-h-svh">
+          <h1 className="text-2xl font-bold uppercase w-full bg-white/80 p-3 text-center">
+            Sistema de Gestão de Manutenção
+          </h1>
+
+          <div>
+            <div className="grid grid-cols-4 gap-4 p-6">
+              {cards.map((props) => (
+                <Card
+                  color={props.color}
+                  qty={props.qty}
+                  text={props.text}
+                  icon={props.icon}
+                />
+              ))}
+            </div>
+          </div>
+          <div className="flex flex-1">
+            <Grafico />
+            <Tabela />
+          </div>
+        </main>
+      </div>
+      <Footer />
+    </div>
+  );
 }
+//   PÁGINA Original !!!!!!!!!
