a774247a3bbb9b23f2e688743ef5720acf46c1a1
andrelbastida <andrelbastida@yahoo.com.br>
2024-08-06 22:32:06 -0300

    chore: no significant changes

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,52 +1,80 @@
-import { Grafico } from "@/app/components/graphics";
-import "../app/globals.css";
-import { Tabela } from "@/app/components/table";
-import { Footer } from "@/app/components/footer";
-import { Card } from "@/app/components/card";
-import { Aside } from "@/app/components/aside";
-import { FaBuilding } from "react-icons/fa";
-import { BotaoBtn } from "@/app/components/botao";
-import { Table } from "@tremor/react";
-import colors from "tailwindcss/colors";
-import { TesteTabela } from "@/app/components/testeTabela";
-// Pagina de Manutenções
+import { Grafico } from '@/app/components/graphics';
+import '../app/globals.css';
+import { Tabela } from '@/app/components/table';
+import { Footer } from '@/app/components/footer';
+import { Card } from '@/app/components/card';
+import { Aside } from '@/app/components/aside';
+import { FaBuilding } from 'react-icons/fa';
+import { BotaoBtn } from '@/app/components/botaoBtn';
+import { IoCloudUpload } from 'react-icons/io5';
+// Pagina de Cadastro de Equipamentos
 
-export default function Index() {
+export default function CadastroEquipamento(){
+    const formulario = [
+        {titulo:"Nome do Equipamento/Ferramenta"},
+        {titulo:"Descrição"},
+        {titulo:"Número de Série/Serial"},
+        {titulo:"Marca/Modelo"},
+        {titulo:"Fabricante"},
+        {titulo:"Data de Aquisição"},
+        {titulo:"Data de Fabricação"},
+        {titulo:"Localização"},
+        {titulo:"Condição"},
+        {titulo:"Valor de Aquisição"},
+        {titulo:"Vida Útil Estimada"},
+        {titulo:"Outros"},
+        ];
   const botoes = [
     {
       color: "white",
-      text: "Cadastrar Manutenção",
+      text: <div className="flex flex-1 gap-2"> <h4>Upload</h4> <IoCloudUpload /></div>,
     },
+    {
+      color: "white",
+      text: "Enviar",
+    },
+    
   ];
-
-  const titulo = ["Maquina", "Local" , "Encarregado", "Status" , "Relatório", "Ações"];
-  const conteudo = [
-    ["Multilazer","Setor A C3", "Setor A", "Em Andamento", "Pdf", "Icones"],
-   
-  ];
-
   return (
     <div className="h-screen flex flex-col">
       <div className="flex-1 flex">
-        <Aside />
+        <Aside/>
 
         <main className="flex-1 flex-col flex text-black max-h-svh">
           <h1 className="text-2xl font-bold uppercase w-full bg-white/80 p-3 text-center">
-            Manutenções
+            Cadastro de Equipamentos
           </h1>
-          <div className="paginaMaintenance-data">
-            {/* Conteudo do site abaixo */}
-            {botoes.map((props) => (
-              <BotaoBtn color={props.color} text={props.text} />
-            ))}
-            {botoes.map((props) => (
-              <BotaoBtn color={props.color} text={props.text} />
-            ))}
+          <div className="space-y-2 form-data">
+
+            <h3>Selecione o tipo:</h3>
+            <select className='w-40'>
+                <option value="ferramenta">Ferramenta</option>
+                <option value="epi">EPI</option>
+                <option value="equipamento">Equipamento</option>
+            </select>
+      
+
+          <form  className="space-y-2 form-data">
+          {formulario.map((obj, index) => (
+                            <div key={index}>
+                              <label >{obj.titulo}</label>
+                              <input />
+                            </div>
+                            
+                           
+                        ))}
+           
+           <div className="">
+              {botoes.map((props) => (
+                <BotaoBtn color={props.color} text={props.text} />
+              ))}
+            </div>
+        </form>
           </div>
-          <TesteTabela titulos={titulo}  campos={conteudo}/>
           
         </main>
       </div>
+      
     </div>
   );
 }
