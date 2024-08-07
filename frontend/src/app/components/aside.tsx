c516f5c9815b35f54c6f17b0e648b00a72b65745
andrelbastida <andrelbastida@yahoo.com.br>
2024-08-06 21:42:59 -0300

    refactor: update image reference in file

--- frontend/src/app/components/aside.tsx
+++ frontend/src/app/components/aside.tsx
@@ -5,7 +5,7 @@ export function Aside(){
     return(
 
         <aside className="w-48 p-6 bg-white/80 text-black">
-        <Image src={"/image/logo"} className="w-full"
+        <Image src={"/image/logo.png"} className="w-full"
         alt="Logo" width={300} height={300}/>
         
         <NavBar/>
